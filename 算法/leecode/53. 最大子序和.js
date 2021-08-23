851b7dae4e4600a1a0ccaf685620deb0c6ae13d3
Rabbitzzc <zzclovelcs@gmail.com>
2021-08-23 19:49:14 +0800

    feat: lt 198. 打家劫舍

--- 算法/leecode/53. 最大子序和.js
+++ 算法/leecode/53. 最大子序和.js
@@ -1,24 +1,16 @@
 // 最大子序列和
 /**
- * O(n) 思路：对于前面的子序列，如果>0，则可以需要，如果<0，则需要丢弃
+ * O(n) 思路：对于前面的子序列，如果>0，则可以需要，如果<0，则需要丢弃，也就是动态规划
+ * dp[n] = dp[n-1] + nums[n] dp[n-1]>0
+ * dp[n] = nums[n]  dp[n-1] < 0
  */
 var maxSubArray = function (nums) {
-    let res = nums[0];
-    let sum = 0;
-    for (let num of nums) {
-        if (sum > 0)
-            sum += num;
-        else
-            sum = num;
-        res = Math.max(res, sum);
-    }
-    return res;
-};
-
-arr = [4, 3, -8, 9]
-
-console.log(maxSubArray(arr))
-
-
-// 分而治之方法
-// 先分解，计算，再合并
+  let res = nums[0]
+  let sum = 0
+  for (let num of nums) {
+    if (sum > 0) sum += num
+    else sum = num
+    res = Math.max(res, sum)
+  }
+  return res
+}
