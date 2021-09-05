d23a4b6df5e5e71b2e0e589e081d073b9c9c6674
Rabbitzzc <zzclovelcs@gmail.com>
2021-09-05 15:15:58 +0800

    feat: 二叉树遍历

--- 算法/leecode/145. 二叉树的后序遍历.js
+++ 算法/leecode/145. 二叉树的后序遍历.js
@@ -27,6 +27,7 @@ var postorderTraversal = function (root) {
  * 递归可能比较简单，有没有迭代的过程，利用 dfs + 栈（现金后出）
  * 递归是向上回溯访问的，而迭代不能返回，因此可以将思路返过来
  * 后续为 左右根  反过来即为根右左
+ * 利用 JS，可以实现 UNshift
  */
 
  var postorderTraversal = function (root) {
@@ -36,13 +37,49 @@ var postorderTraversal = function (root) {
     let stack = [root]
     while(stack.length) {
         // 吐出根
-        const item = stack.pop();
-        if(item) {
-            res.push(item.val)
-            stack.push(item.left);
-            stack.push(item.right);
-        }
+        const temp = stack.pop()
+        res.push(temp.val)
+        if(temp.left) stack.push(temp.left)
+        if(temp.right) stack.push(temp.right)
     }
     
-    return res.reverse();
+    return res.reverse()
 };
+
+// 利用 unshift，避免 reverse
+const postorderTraversal = root => {
+    const res = [];
+    if(!root) return res;
+
+    while(stack.length) {
+        const temp = stack.pop()
+        res.unshift(temp.val)
+        if(temp.left) stack.push(temp.left)
+        if(temp.right) stack.push(temp.right)
+    }
+    return res
+}
+
+/**
+ * 跟前序中序，类似，也可以使用迭代的方式去实现，可以先直接遍历到右边
+ */
+ const postorderTraversal = root => {
+    const res = [];
+    if(!root) return res;
+
+    let now  = root
+    const stack = []
+
+    while(now || stack.length) {
+        while(now) {
+            res.unshift(now.val)
+            stack.push(now)
+            now = now.right
+        }
+
+        const temp = stack.pop()
+        temp.left  && (now = temp.left)
+    }  
+
+    return res 
+ }
